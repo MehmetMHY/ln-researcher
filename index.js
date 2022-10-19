5e7b595ace841cdb2b21e65ed289d7278fadf070
mehmet-mhy <mehmet.mhy@gmail.com>
2022-10-19 17:03:04 +0300

    started the development of the image/ endpoint. checkout docs/sources.txt to view all the sources I used

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
-// SOURCE: https://expressjs.com/en/starter/hello-world.html
-
+const Joi = require("joi")
+const CryptoJS = require("crypto-js");
+const crypto = require("crypto");
+const fs = require("fs");
 const express = require('express')
-const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const app = express()
+app.use(express.json())
+
+app.post('/', (req, res) => {
+  let img = __dirname + "/data/img_1155.jpg";
+
+  const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+      modulusLength: 2048,
+  });
+
+  fs.readFile(img, function(err, content) {
+      if (err) {
+          console.log(err)
+      } else {
+          const key = crypto.randomBytes(64).toString('hex')
+
+          let output = {
+              "image": CryptoJS.Rabbit.encrypt(content, key),
+              "encKey": crypto.publicEncrypt(
+                  {
+                      key: publicKey,
+                      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+                      oaepHash: "sha256",
+                  },
+                  Buffer.from(key)
+              )
+          }
+          res.writeHead(200, { "Content-type": "application/json" });
+          res.write(JSON.stringify(output));
+          res.end();
+      }
+  })
 })
 
+// run server
+const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}. Check out http://localhost:${port} if you running this API locally.`)
 })
+
 
