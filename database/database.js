40a99e09950cf6bf68fe6537838cadf4401a26d9
mehmet-mhy <mehmet.mhy@gmail.com>
2022-10-22 20:59:56 +0300

    added remove() function to database.js and cleaned up the code a bit

--- database/database.js
+++ database/database.js
@@ -10,6 +10,20 @@ async function isObject(value){
     return true
 }
 
+async function jsonToAndStaement(targets){
+    let reTargets = ""
+    let counter = 0
+    for ( let key of Object.keys(targets) ) {
+        reTargets = reTargets + `data->>'${key}' = '${targets[key]}'`
+        if ( counter !== Object.keys(targets).length - 1 ){
+            reTargets += " AND "
+        }
+        counter += 1
+    }
+
+    return reTargets
+}
+
 async function add(data){
     const output = { "status": 0, "errors": [], "result": [] }
     
@@ -47,16 +61,7 @@ async function get(targets=undefined){
             return output
         }
     
-        let reTargets = ""
-        let counter = 0
-        for ( let key of Object.keys(targets) ) {
-            reTargets = reTargets + `data->>'${key}' = '${targets[key]}'`
-            if ( counter !== Object.keys(targets).length - 1 ){
-                reTargets += " AND "
-            }
-            counter += 1
-        }
-
+        let reTargets = await jsonToAndStaement(targets)
         sqlCmd = `SELECT * FROM images WHERE ${reTargets}`
     }
 
@@ -74,10 +79,32 @@ async function get(targets=undefined){
     return output
 }
 
-// // MAIN FUNCTION CALLS
+async function remove(targets=undefined){
+    const output = { "status": 0, "errors": [], "result": [] }
+    
+    if ( !isObject(targets) || Object.keys(targets).length === 0 ) {
+        output["status"] = -1
+        output["errors"].push(`Inputed data is NOT a valid object/json: ${JSON.stringify(targets)}`)
+        return output
+    }
 
-// data = { "id": 1, "filepath": "/opt/data/img.png", "status": "labeled", "uploaded": 1666292194, "completed": 1676292194, "labelData": {} }
-// add(data).then(result => console.log(JSON.stringify(result, null, indent=4)))
-// get( { "id": 1, "filepath": "/opt/data/img.png" } ).then(result=>console.log(result, null, indent=4))
+    const reTargets = await jsonToAndStaement(targets)
+    const sqlCmd = `DELETE FROM images WHERE ${reTargets}`
 
+    try {
+        const result = await pool.query(sqlCmd)
+        await pool.end()
+        output["result"] = result
+    } catch(e) {
+        output["status"] = -1
+        output["errors"].push(`${e}`)
+    }
 
+    return output
+}
+
+module.exports = {
+    get,
+    add,
+    remove
+}
