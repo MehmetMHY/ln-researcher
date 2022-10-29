fd4f58d5b39d7d2a6c571523410df92680d5ac53
Dylan Eck <akdylaneck@gmail.com>
2022-10-28 19:27:54 -0600

    add ability to deploy using temporary dev account

--- near/utils/manage-sc.js
+++ near/utils/manage-sc.js
@@ -1,4 +1,5 @@
-import { connect, KeyPair, keyStores, utils } from "near-api-js";
+import NearAPI, { Near } from "near-api-js";
+const { connect, KeyPair, keyStores, utils } = NearAPI;
 import * as os from "os";
 import * as path from "path";
 import * as fs from "fs";
@@ -26,41 +27,6 @@ const account_exists = async (account_id, near_connection) => {
     return false;
   }
   return true;
-};
-
-/**
- * Creates a new sub account under the specified account
- * The network (mainnet, testnet, etc) is determined by near_connection
- * @param {Account} account         NEAR account object
- * @param {string}  prefix          prefix to distinguish the sub account
- * @param {Near}    near_connection near connection object
- */
-const create_sub_account = async (account, prefix, near_connection) => {
-  const main_account_id = account.accountId;
-  const sub_account_id = `${prefix}.${main_account_id}`;
-
-  // create public / private key pair
-  const key_pair = await KeyPair.fromRandom("ed25519");
-  const public_key = keyPair.getPublicKey();
-
-  // write keys to local storage
-  await near_connection.connection.signer.keyStore.setKey(
-    "testnet",
-    sub_account_id,
-    key_pair
-  );
-
-  // create sub account
-  try {
-    const response = await account.createAccount(
-      sub_account_id,
-      public_key,
-      utils.format.parseNearAmount("200")
-    );
-    console.log(response);
-  } catch (err) {
-    throw err;
-  }
 };
 
 /**
@@ -106,43 +72,159 @@ const authenticate_user = async (account_id) => {
   }
 };
 
-// prompt user for account id
-const { account_id } = await inquirer.prompt([
-  {
-    type: "input",
-    name: "account_id",
-    message: "enter you near account id (testnet): ",
-  },
-]);
+/**
+ * Creates a new sub account under the specified account
+ * The network (mainnet, testnet, etc) is determined by near_connection
+ * @param {Account} account         NEAR account object
+ * @param {string}  prefix          prefix to distinguish the sub account
+ * @param {Near}    near_connection near connection object
+ */
+const create_sub_account = async (account, prefix, near_connection) => {
+  const main_account_id = account.accountId;
+  const sub_account_id = `${prefix}.${main_account_id}`;
 
-// set up near connection and authenticate user
-const near_connection = await connect_to_near();
-await authenticate_user(account_id);
-const account = await near_connection.account(account_id);
+  // create public / private key pair
+  const key_pair = await KeyPair.fromRandom("ed25519");
+  const public_key = keyPair.getPublicKey();
 
-// configure sub account
-const sub_account_id = `sc.${account_id}`;
-if (!(await account_exists(sub_account_id, near_connection))) {
-  console.log("creating sub account for smart contract deployment");
-  await create_sub_account(account, "sc", near_connection);
-} else {
-  console.log("found existing sub account");
-}
-const sub_account = await near_connection.account(sub_account_id);
-
-// check if smart contract has been deployed to sub account
-const state = await sub_account.state();
-let sc_deployed = state.code_hash != "11111111111111111111111111111111";
-
-// deploy smart contract if necessary
-if (!sc_deployed) {
-  console.log("deploying smart contract to sub account");
-  // build smart contract
-  execSync("cd ../contract && npm run build");
-  const response = await sub_account.deployContract(
-    fs.readFileSync("../contract/build/job_posting.wasm")
+  // write keys to local storage
+  await near_connection.connection.signer.keyStore.setKey(
+    "testnet",
+    sub_account_id,
+    key_pair
   );
-  console.log(response);
-} else {
-  console.log("smart contract already deployed");
-}
+
+  // create sub account
+  try {
+    const response = await account.createAccount(
+      sub_account_id,
+      public_key,
+      utils.format.parseNearAmount("200")
+    );
+    console.log(response);
+  } catch (err) {
+    throw err;
+  }
+};
+
+/**
+ * Check for an existing sub account, create a new one if necessary
+ * @param   {string} account_id      id of the main account
+ * @param   {Near}   near_connection NEAR connection object
+ * @returns                          NEAR account object
+ */
+const configure_sub_account = async (account_id, near_connection) => {
+  await authenticate_user(account_id);
+  const account = await near_connection.account(account_id);
+
+  // configure sub account
+  const sub_account_id = `sc.${account_id}`;
+  if (!(await account_exists(sub_account_id, near_connection))) {
+    console.log("creating sub account for smart contract deployment");
+    await create_sub_account(account, "sc", near_connection);
+  } else {
+    console.log("found existing sub account");
+  }
+
+  const sub_account = await near_connection.account(sub_account_id);
+  return sub_account;
+};
+
+/**
+ * Check for an existing dev account, create a new one if necessary
+ * @param   {Near} near_connection Near connection object
+ * @returns                        Near account object
+ */
+const configure_dev_account = async (near_connection) => {
+  const account_file_path = path.join("neardev", "dev-account.json");
+  const account_env_path = path.join("neardev", "dev-account.env");
+
+  const key_store = near_connection.connection.signer.keyStore;
+
+  try {
+    const dev_account_id = fs
+      .readFileSync(account_file_path)
+      .toString("utf8")
+      .trim();
+    fs.readFileSync(account_env_path);
+
+    if (dev_account_id && (await key_store.getKey("testnet", dev_account_id))) {
+      console.log(`using existing dev account ${dev_account_id}`);
+      const dev_account = await near_connection.account(dev_account_id);
+      return dev_account;
+    }
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      if (!fs.existsSync("neardev")) {
+        fs.mkdirSync("neardev");
+      }
+    } else {
+      throw err;
+    }
+  }
+
+  console.log("creating new dev account");
+  const randomNumber = Math.floor(
+    Math.random() * (99999999999999 - 10000000000000) + 10000000000000
+  );
+  const dev_account_id = `dev-${Date.now()}-${randomNumber}`;
+
+  const keyPair = await KeyPair.fromRandom("ed25519");
+  await near_connection.createAccount(dev_account_id, keyPair.getPublicKey());
+  await key_store.setKey("testnet", dev_account_id, keyPair);
+  fs.writeFileSync(account_file_path, dev_account_id);
+  fs.writeFileSync(account_env_path, `CONTRACT_NAME=${dev_account_id}`);
+
+  const dev_account = await near_connection.account(dev_account_id);
+  return dev_account;
+};
+
+/**
+ * Deploy smart contract to specified account
+ * Smart contract is currently hard coded
+ * @param {Account} account Near account object
+ */
+const deploy_smart_contract = async (account) => {
+  // check if smart contract has been deployed to account
+  const state = await account.state();
+  let sc_deployed = state.code_hash != "11111111111111111111111111111111";
+
+  // deploy smart contract if necessary
+  if (!sc_deployed) {
+    console.log("deploying smart contract to sub account");
+    // build smart contract
+    execSync("cd ../contract && npm run build");
+    const response = await sub_account.deployContract(
+      fs.readFileSync("../contract/build/job_posting.wasm")
+    );
+    console.log(response);
+  } else {
+    console.log("smart contract already deployed");
+  }
+};
+
+/**
+ * Main funciton to handle overall program flow
+ */
+const main = async () => {
+  // prompt user for account id
+  const { account_id } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "account_id",
+      message: "enter you near account id (testnet only, blank for dev): ",
+    },
+  ]);
+
+  // set up near connection and authenticate user
+  const near_connection = await connect_to_near();
+  let contract_account;
+  if (account_id) {
+    contract_account = await configure_sub_account(account_id, near_connection);
+  } else {
+    contract_account = await configure_dev_account(near_connection);
+  }
+  await deploy_smart_contract(contract_account);
+};
+
+await main();
