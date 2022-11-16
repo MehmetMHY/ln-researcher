0be0963c74952530f135d209111751f29c96317b
mehmet-mhy <mehmet.mhy@gmail.com>
2022-11-16 02:57:32 -0700

    added getRSAKeys() to crpytography.js

--- controllers/cryptography.js
+++ controllers/cryptography.js
@@ -1,8 +1,20 @@
 const crypto = require("crypto")
 
 /*
-CREDIT
+CREDIT - A function that generates and returns a RSA key pair (public & private keys):
+This function was taken directly and modified from GeeksForGeeks' online article:
+    - 11-4-2022 : https://www.geeksforgeeks.org/node-js-crypto-generatekeypair-method/
 */
+async function getRSAKeys(){
+    const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+        modulusLength: 2048,
+    })
+
+    return {
+        public: publicKey.export({ type: "pkcs1", format: "pem" }),
+        private: privateKey.export({ type: "pkcs1", format: "pem" })
+    }
+}
 
 /*
 CREDIT - Encrpt a buffer given a key and encryption algorithm (or default to aes-256-ctr)
@@ -43,6 +55,7 @@ async function genRandom32BitKey(){
 module.exports = {
     encrypt,
     decrypt,
-    genRandom32BitKey
+    genRandom32BitKey,
+    getRSAKeys
 }
 
