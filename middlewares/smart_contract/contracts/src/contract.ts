d4cecea250fafa1a84e277f121c0f64f0c2961fc
Dylan Eck <akdylaneck@gmail.com>
2022-11-20 19:29:00 -0700

    change greater than to less than

--- middlewares/smart_contract/contracts/src/contract.ts
+++ middlewares/smart_contract/contracts/src/contract.ts
@@ -245,7 +245,7 @@ class JobPosting {
     rsa_pk,
   }: {
     rsa_pk: string;
-  }): { url: string; id: string; task: Task } | string {
+  }): { url: string; id: string; label_keys: string[]; task: Task } | string {
     if (!rsa_pk) {
       return "error: must provide rsa public key";
     }
@@ -299,7 +299,12 @@ class JobPosting {
 
     job.tasks.push(task);
     this.in_progress_jobs.push(job);
-    return { url: this.url, id: job.id, task: task };
+    return {
+      url: this.url,
+      id: job.id,
+      label_keys: job.label_keys,
+      task: task,
+    };
   }
 
   @call({})
